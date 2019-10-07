fa3d3056a0d2b0de428abf66ee3de47468f1db43
Bayu Beta Brahmantio <bayubrahmantio@gmail.com>
2019-10-07 02:09:45 +0200

    edited source

--- R/app.R
+++ R/app.R
@@ -1,6 +1,6 @@
 library(jsonlite)
 library(shiny)
-source("R/population.R")
+source("https://github.com/Jorisvdoorn/lab5group8/R/population.R")
 
 # object of the RC-class 'population' based on the package of lab5group8 that connects to the kolada-api
 kolada <- population$new()
